976501e410881bb390d60b5772d027b298af9e71
SerifOSalem <serif.salem@outlook.com>
2026-03-06 10:55:08 +0000

    Start CatApp - Week 2 Lab - Add eat method

--- src/Cat.kt
+++ src/Cat.kt
@@ -6,6 +6,11 @@ class Cat(val name: String, var age: Int, var weight: Int) {
         weight--
     }
 
+    // NEW METHOD: increases the cat's weight by 1
+    fun eat() {
+        weight++
+    }
+
     override fun toString(): String {
         return "Name:## $name Age: $age Weight: $weight"
     }
--- src/CatDemo.kt
+++ src/CatDemo.kt
@@ -1,23 +1,18 @@
 
-// This file demonstrates OBJECT creation and usage
 fun main() {
-
-    // OBJECT: An object is a specific instance of a class.
-
-    // Here, binnie and clyde are objects of the Cat class.
-    // We are calling the CONSTRUCTOR to create them.
-
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
 
-    // Calling a METHOD on each object
+    // Calling walk method on each object
     binnie.walk()
     clyde.walk()
 
-    /* println(objectName)
-        automatically calls the toString() method
-        to determine how the object should be displayed. */
+    // Calling eat method on each object
+    binnie.eat()
+    clyde.eat()
 
+    // println(objectName) - automatically calls the toString() method
+    // to determine how the object should be displayed.
     println(binnie)
     println(clyde)
 }
